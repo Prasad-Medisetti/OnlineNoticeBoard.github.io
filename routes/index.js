0fdc8126937acc51ad1083632b272aded26b1701
Prasad Medisetti <53884322+Prasad-Medisetti@users.noreply.github.com>
2020-05-20 22:00:34 +0530

    20-05-2020

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var monk = require('monk');
 var QRCode = require('qrcode');
+var db = monk('localhost:27017/codeheat');
+var image = db.get('image');
 var multer = require('multer');
-var upload = multer({ dest : 'uploads/' });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+var upload = multer({ storage: storage });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,8 +38,15 @@ router.get('/admin', function(req, res, next) {
   res.render('adminlogin');
 });
 
-router.get('/image', function(req, res, next) {
-  res.render('image');
+router.get('/admindashboard', function(req, res, next) {
+  res.render('admindashboard');
+});
+
+router.get('/image', function(req,res){
+  image.find({}, function(err,docs){
+    console.log(docs);
+    res.render('image', {"a":docs});
+  })
 });
 
 router.post('/imageupload', upload.single('image'), function(req, res) {
